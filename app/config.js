bd32afe0f12529c98eb588d123fe5160fce7c38c
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2022-12-16 16:27:25 +0000

    update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Teacher self-service prototype',
 
   // Default port that prototype runs on
   port: '3000',
